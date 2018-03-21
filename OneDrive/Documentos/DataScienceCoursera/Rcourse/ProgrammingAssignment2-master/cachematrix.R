983fe5567c4e0882d8525ecbe70ec02e3302ad88
cinth <cinthya.villafana@gmail.com>
2018-03-21 11:00:51 +0100

    Commit with explanatory comments

--- OneDrive/Documentos/DataScienceCoursera/Rcourse/ProgrammingAssignment2-master/cachematrix.R
+++ OneDrive/Documentos/DataScienceCoursera/Rcourse/ProgrammingAssignment2-master/cachematrix.R
@@ -4,20 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
+    inv <- NULL
+    ## x is initialized as a function agrgument, with a default value (empty numeric vector)
+    ## sets de 'inverse' to NULL as a placeholder for a future value
+    set <- function(y){
     x <<- y
     inv <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
+    }
+    ## set function, assigns the infput argument to x in the PARENT ENV.
+    ## and assigns NULL to m to clear previous vlues
+  get <- function() x ## getter for x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix ## setter for the inverse
+  getInverse <- function() inv ## getter for the inverse
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  ## creates the list that will be the input for cachesolve.r
 
 }
 
 
-## Write a short comment describing this function
+## this function takes the makeCacheMatrix as input, and returns the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
